9bcfa035a99de511d762f04b2ced4d84a474014e
Toukinho <jonhlukelima@gmail.com>
2026-04-27 10:25:37 -0300

    chore: atualizar configurações e rotas da aplicação
    - Atualizar AppServiceProvider
    - Atualizar bootstrap/app.php
    - Atualizar rotas web

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
+use App\Models\Url;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/{code}', function (string $code) {
+    $url = Url::where('short_url', $code)->firstOrFail();
+    $url->increment('access_count');
+    return redirect($url->url, 301);
 });
